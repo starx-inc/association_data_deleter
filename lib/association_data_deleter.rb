01a5df9b4c071c8c70d26100e895434efb7113d7
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-02-14 17:16:08 +0900

    feat: Initial commit. create gem skeleton by Bundler

---
+++ lib/association_data_deleter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "association_data_deleter/version"
+
+module AssociationDataDeleter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/association_data_deleter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AssociationDataDeleter
+  VERSION = "0.1.0"
+end
