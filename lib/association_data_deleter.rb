856b29e12b66770cb0d468fad1d5eaf7fcf62cec
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-05 16:45:18 +0900

    fix: 接頭辞を空文字に指定

--- lib/association_data_deleter.rb
+++ lib/association_data_deleter.rb
@@ -10,5 +10,10 @@ require_relative "association_data_deleter/models/deletion_job_detail"
 
 module AssociationDataDeleter
   class Error < StandardError; end
+  # 名前空間接頭辞を空文字列に設定して無効化
+  def self.table_name_prefix
+    ""
+  end
+
   # Your code goes here...
 end
