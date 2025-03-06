99eb4ab7c30841706b088130bd84f8bf83302de5
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 16:35:43 +0900

    remove: ralitie.rb

--- lib/association_data_deleter/railtie.rb
+++ lib/association_data_deleter/railtie.rb
@@ -4,15 +4,6 @@ if defined?(Rails)
       rake_tasks do
         load File.expand_path('../../tasks/association_data_deleter.rake', __FILE__)
       end
-      
-      initializer "association_data_deleter.configure_rails_initialization" do
-        Rails.application.config.after_initialize do
-          Rails.application.routes.append do
-            # ホストアプリケーションで以下のようにマウントできるようにする
-            # mount AssociationDataDeleter::Engine, at: "/association_data_deleter"
-          end
-        end
-      end
     end
   end
 end
