d9a640605bc5823c642c9f7b763244ef360200c0
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 16:29:13 +0900

    fix: lib

--- lib/association_data_deleter.rb
+++ lib/association_data_deleter.rb
@@ -9,12 +9,5 @@ require_relative "association_data_deleter/models/deletion_job"
 require_relative "association_data_deleter/models/deletion_job_detail"
 
 module AssociationDataDeleter
-  class Error < StandardError; end
-  
-  # 名前空間のテーブル名プレフィックスを無効化
-  if defined?(ActiveRecord)
-    ActiveRecord::Base.class_eval do
-      self.table_name_prefix = '' if self.name && self.name.start_with?('AssociationDataDeleter::')
-    end
-  end
+  class Error < StandardError;
 end
