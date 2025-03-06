a3fadcb68378d50f4bb398a5a851a2b188731948
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 15:44:35 +0900

    remove: エラー処理、ルーティング

--- lib/association_data_deleter/controllers/deletion_jobs_controller.rb
+++ lib/association_data_deleter/controllers/deletion_jobs_controller.rb
@@ -1,19 +1,8 @@
 module AssociationDataDeleter
   class DeletionJobsController < AssociationDataDeleter::ApplicationController
     def index
-      # より直接的な方法でデータを取得
-      begin
-        # テーブル名を明示的に指定したクエリを使用
-        @deletion_jobs = AssociationDataDeleter::DeletionJob.all.order(created_at: :desc)
-      rescue ActiveRecord::StatementInvalid => e
-        # エラーが発生した場合は接続を使って直接クエリを実行
-        connection = ActiveRecord::Base.connection
-        @deletion_jobs = connection.exec_query("SELECT * FROM deletion_jobs ORDER BY created_at DESC").map do |row|
-          DeletionJob.new(row)
-        end
-      end
-      
-      # 明示的にテンプレートを指定
+      @deletion_jobs = AssociationDataDeleter::DeletionJob.all.order(created_at: :desc)
+
       render template: "deletion_jobs/index", layout: 'application'
     end
   end
