60535d5c5d685adb54c31a18ba742f825c84e45b
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 16:33:57 +0900

    remove: model

--- lib/association_data_deleter/models/deletion_job.rb
+++ lib/association_data_deleter/models/deletion_job.rb
@@ -1,12 +1,5 @@
 module AssociationDataDeleter
   class DeletionJob < ActiveRecord::Base
-    # テーブル名を明示的に指定（クラスメソッドとして定義して強制）
-    def self.table_name
-      "deletion_jobs"
-    end
-    
-    has_many :deletion_job_details, 
-             class_name: "AssociationDataDeleter::DeletionJobDetail",
-             dependent: :destroy
+    has_many :deletion_job_details, dependent: :destroy
   end
 end
--- lib/association_data_deleter/models/deletion_job_detail.rb
+++ lib/association_data_deleter/models/deletion_job_detail.rb
@@ -1,11 +1,5 @@
 module AssociationDataDeleter
   class DeletionJobDetail < ActiveRecord::Base
-    # テーブル名を明示的に指定（クラスメソッドとして定義して強制）
-    def self.table_name
-      "deletion_job_details"
-    end
-    
-    belongs_to :deletion_job, 
-               class_name: "AssociationDataDeleter::DeletionJob"
+    belongs_to :deletion_job
   end
 end
