d8efe98d78b65123e80d73e134d62d44c16d7d19
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 16:19:30 +0900

    fix: application

--- lib/association_data_deleter/controllers/application_controller.rb
+++ lib/association_data_deleter/controllers/application_controller.rb
@@ -3,15 +3,11 @@ module AssociationDataDeleter
     layout 'application'
 
     before_action :set_view_paths
-
-    def view_paths_for_association_data_deleter
+    
+    def set_view_paths
       gem_path = Gem::Specification.find_by_name('association_data_deleter').gem_dir
       view_path = File.join(gem_path, 'lib', 'association_data_deleter', 'views')
       prepend_view_path(view_path)
-    end
-    
-    def set_view_paths
-      view_paths_for_association_data_deleter
     end
   end
 end 
