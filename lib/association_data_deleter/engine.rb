c9961f3e2697ef72585d676d9dcd3ce64babfc0e
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 16:01:12 +0900

    fix: engine

--- lib/association_data_deleter/engine.rb
+++ lib/association_data_deleter/engine.rb
@@ -1,15 +1,5 @@
 module AssociationDataDeleter
   class Engine < ::Rails::Engine
     config.engine_name = "association_data_deleter"
-
-    initializer "association_data_deleter.assets.precompile" do |app|
-      app.config.assets.precompile += %w( association_data_deleter/application.css )
-    end
-    
-    config.to_prepare do
-      Dir.glob(Engine.root.join("lib/association_data_deleter/controllers/**/*.rb")).each do |c|
-        require_dependency(c)
-      end
-    end
   end
 end
