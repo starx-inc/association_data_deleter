ca7462ac7cba25a9be5081d3e903510efd14f864
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-06 16:04:35 +0900

    feat: engine

--- lib/association_data_deleter/engine.rb
+++ lib/association_data_deleter/engine.rb
@@ -1,5 +1,11 @@
 module AssociationDataDeleter
   class Engine < ::Rails::Engine
     config.engine_name = "association_data_deleter"
+    
+    config.to_prepare do
+      Dir.glob(Engine.root.join("lib/association_data_deleter/controllers/**/*.rb")).each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
