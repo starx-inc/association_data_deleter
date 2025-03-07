18be49bcf74a1fbf9639d833f9425da8355418af
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-07 15:20:18 +0900

    feat: index画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AssociationDataDeleter::Engine.routes.draw do
   # 名前空間を明示的に指定してルーティングを設定
   scope module: 'association_data_deleter' do
-    resources :deletion_jobs, only: %i(index)
+    resources :deletion_jobs, only: %i(index show)
   end
 end 
--- lib/association_data_deleter/controllers/deletion_jobs_controller.rb
+++ lib/association_data_deleter/controllers/deletion_jobs_controller.rb
@@ -5,5 +5,12 @@ module AssociationDataDeleter
 
       render template: "deletion_jobs/index", layout: 'application'
     end
+
+    def show
+      @deletion_job = AssociationDataDeleter::DeletionJob.find(params[:id])
+      @deletion_job_details = @deletion_job.deletion_job_details
+
+      render template: "deletion_jobs/show", layout: 'application'
+    end
   end
 end 
