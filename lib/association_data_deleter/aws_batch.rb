8e8a352c853bca4a941577fa5e101e3d81b3a2cb
atakai <takai1985@gmail.com>
2025-03-11 17:38:32 +0900

    feat: submit_preparation_jobメソッドを追加

--- lib/association_data_deleter/aws_batch.rb
+++ lib/association_data_deleter/aws_batch.rb
@@ -5,5 +5,17 @@ module AssociationDataDeleter
     def initialize
       @client = Aws::Batch::Client.new(region: 'ap-northeast-1')
     end
+
+    def submit_preparation_job(job_name, job_queue, job_definition, deletion_job_id)
+      response = @client.submit_job({
+        job_name: job_name,
+        job_queue: job_queue,
+        job_definition: job_definition,
+        parameters: {
+          'deletion_job_id' => deletion_job_id
+        }
+      })
+      response.job_id
+    end
   end
 end
