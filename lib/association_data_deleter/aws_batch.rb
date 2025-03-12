e42ad41ca8dc365e5bc6de5b553ba849c5aba966
atakai <takai1985@gmail.com>
2025-03-12 13:40:01 +0900

    feat: submit_execution_jobメソッドを追加

--- lib/association_data_deleter/aws_batch.rb
+++ lib/association_data_deleter/aws_batch.rb
@@ -7,10 +7,20 @@ module AssociationDataDeleter
     end
 
     def submit_preparation_job(job_name, deletion_job_id)
+      submit_job(job_name, AssociationDataDeleter.config.preparation_job_queue, AssociationDataDeleter.config.preparation_job_definition, deletion_job_id)
+    end
+
+    def submit_execution_job(job_name, deletion_job_id)
+      submit_job(job_name, AssociationDataDeleter.config.execution_job_queue, AssociationDataDeleter.config.execution_job_definition, deletion_job_id)
+    end
+
+    private
+
+    def submit_job(job_name, job_queue, job_definition, deletion_job_id)
       response = @client.submit_job({
         job_name: job_name,
-        job_queue: AssociationDataDeleter.config.preparation_job_queue,
-        job_definition: AssociationDataDeleter.config.preparation_job_definition,
+        job_queue: job_queue,
+        job_definition: job_definition,
         parameters: {
           'deletion_job_id' => deletion_job_id
         }
