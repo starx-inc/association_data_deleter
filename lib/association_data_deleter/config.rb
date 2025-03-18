aacc57b606b4051a89c5f2631568df2badf78448
atakai <takai1985@gmail.com>
2025-03-18 13:20:45 +0900

    feat: Added authentication config

--- lib/association_data_deleter/config.rb
+++ lib/association_data_deleter/config.rb
@@ -10,12 +10,16 @@ module AssociationDataDeleter
   end
 
   class Config
-    attr_accessor :preparation_job_queue,
+    attr_accessor :authenticate,
+                  :preparation_job_queue,
                   :preparation_job_definition,
                   :execution_job_queue,
                   :execution_job_definition
 
     def initialize
+      # デフォルトは何もしないProcを設定しておく
+      @authenticate = ->(controller) {}
+
       @preparation_job_queue = 'default-preparation-job-queue'
       @preparation_job_definition = 'default-preparation-job-definition'
       @execution_job_queue = 'default-execution-job-queue'
--- lib/association_data_deleter/controllers/application_controller.rb
+++ lib/association_data_deleter/controllers/application_controller.rb
@@ -2,12 +2,20 @@ module AssociationDataDeleter
   class ApplicationController < ::ApplicationController
     layout 'application'
 
+    before_action :authenticate_host_app!
     before_action :set_view_paths
 
     def set_view_paths
       gem_path = Gem::Specification.find_by_name('association_data_deleter').gem_dir
       view_path = File.join(gem_path, 'lib', 'association_data_deleter', 'views')
       prepend_view_path(view_path)
+    end
+
+    private
+
+    def authenticate_host_app!
+      # ホストアプリから渡された認証Procを実行する
+      AssociationDataDeleter.config.authenticate.call(self)
     end
   end
 end 
