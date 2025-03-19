71130f544c60f11144646757910f187437ca0338
atakai <takai1985@gmail.com>
2025-03-19 16:49:11 +0900

    feat: Added execute action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 AssociationDataDeleter::Engine.routes.draw do
   # 名前空間を明示的に指定してルーティングを設定
   scope module: 'association_data_deleter' do
-    resources :deletion_jobs, only: %i(index show create)
+    resources :deletion_jobs, only: %i(index show create) do
+      post :execute, on: :member
+    end
   end
 end 
