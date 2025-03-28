1cdbda39bdee6dd00037db7ccf316091b517a036
Kanata Hiraguchi <k-hiraguchi@starx.co.jp>
2025-03-28 15:26:35 +0900

    feat: add exists_for_target? method

--- lib/association_data_deleter/models/deletion_job.rb
+++ lib/association_data_deleter/models/deletion_job.rb
@@ -1,5 +1,9 @@
 module AssociationDataDeleter
   class DeletionJob < ActiveRecord::Base
     has_many :deletion_job_details, dependent: :destroy
+    
+    def self.exists_for_target?(target_id, target_type)
+      exists?(target_id: target_id, target_type: target_type)
+    end
   end
 end
